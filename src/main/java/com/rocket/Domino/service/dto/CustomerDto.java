14c9abf19e470bf6760d0184b10c5838faf39320
DavidLipaDevp <cancer123david@gmail.com>
2023-10-12 00:31:53 -0500

    Update prueba

---
+++ src/main/java/com/rocket/Domino/service/dto/CustomerDto.java
@@ -0,0 +1,7 @@
+package com.rocket.Domino.service.dto;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+public class CustomerDto {
+   private final String HOLA="FJDFFASDAS";
+}
